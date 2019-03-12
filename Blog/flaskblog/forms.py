938b7e6e891bc44be5735eb62ccabe5a8da8bbd8
amyard <maksymturenko@gmailcom>
2019-03-12 22:14:17 +0300

    'db_and_authentication'

--- Blog/flaskblog/forms.py
+++ Blog/flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 # DataRequired - variable can not be empty
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,17 @@ class RegistrationForm(FlaskForm):
 	password = PasswordField('Password', validators = [DataRequired()])
 	confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign Up')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user:
+			raise ValidationError('That username is taken. Choose another name.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email = email.data).first()
+		if user:
+			raise ValidationError('That email is taken. Choose another email.')
+
 
 
 class LoginForm(FlaskForm):
